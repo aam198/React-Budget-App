49900b5294592f2d78bc7dc241764b7470a8b073
aam198 <aam198@psu.edu>
2021-03-19 15:30:25 -0400

    Beginning build of budget app

---
+++ src/components/Balance.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Balance() {
+    return (
+        <>
+            <div className="container">
+                <h4>Your Balance</h4>
+                <h2>$0.00</h2>
+            </div>
+        </>
+    )
+}
