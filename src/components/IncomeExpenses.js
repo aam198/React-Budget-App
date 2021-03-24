154e612d57f8c1ed0af24860ca9f72ecd23b6642
aam198 <aam198@psu.edu>
2021-03-24 15:50:25 -0400

    updates to add transaction, action and styling.

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -7,7 +7,7 @@ export const IncomeExpenses = () => {
 
     const amounts = transactions.map(transaction => transaction.amount);
 
-    const income = amounts
+    const income = amounts 
         .filter(item => item > 0)
         .reduce((acc, item) => (acc += item), 0)
         .toFixed(2);
