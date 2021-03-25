964af0d79ff295b747210f4843ff3a28feb7613a
aam198 <aam198@psu.edu>
2021-03-25 08:46:06 -0400

    fix: clear out input form after submit

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -21,15 +21,9 @@ export const AddTransaction = ()=> {
         addTransaction(newTransaction);
 
         // clearing out the input form
-        setText({
-            text: ' '
-        });
+        setAmount('');
 
-        setAmount({
-            amount: ''
-        });
-
-        
+        setText('');
 
 
      }
@@ -43,6 +37,7 @@ export const AddTransaction = ()=> {
                 <label htmlFor="text">Text</label>
                 <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
                 </div>
+                
                 <div className="form-control">
                 <label htmlFor="amount">Amount <br />
                     (negative - expense, positive - income)</label
