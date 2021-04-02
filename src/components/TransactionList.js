f0d868e1d90cd3fce4040c1c21f5c357d05a2cde
aam198 <aam198@psu.edu>
2021-04-02 15:26:08 -0400

    style: className add, transactionlist li

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -7,12 +7,12 @@ export default function TransactionList() {
     const { transactions } = useContext(GlobalContext);
 
     return (
-        <>
+        <div className="history-container">
             <h3>History</h3>
             <ul className="list">
                 {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
                 
             </ul>
-        </>
+        </div>
     )
 }
