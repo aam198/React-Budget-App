a419fb685d4c8fbfb7c8dffd613d07f511925b2c
aam198 <aam198@psu.edu>
2021-04-06 14:54:04 -0400

    finished: controller setup, added: get,post,delete routes.

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -5,3 +5,18 @@
 exports.getTransactions = (req, res, next) => {
     res.send('GET transactions');
 } 
+
+// @desc     Add transaction 
+// @route    POST /api/v1/transactions
+// @access   Public
+exports.addTransaction = (req, res, next) => {
+    res.send('POST transaction');
+} 
+
+// @desc     Delete transaction
+// @route    DELETE /api/v1/transaction/:id
+// @access   Public
+exports.deleteTransaction = (req, res, next) => {
+    res.send('DELETE transaction');
+} 
+
