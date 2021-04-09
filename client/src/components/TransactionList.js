b01ba7f6686c1e5db14d1790bb81ddcc9d9ddb34
aam198 <aam198@psu.edu>
2021-04-09 14:13:14 -0400

    update: transactionList with useEffect hook

--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -1,11 +1,15 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { GlobalContext } from '../context/GlobalState';
 import { Transaction } from './Transaction';
 
 export default function TransactionList() {
 
-    const { transactions } = useContext(GlobalContext);
+    const { transactions, getTransactions } = useContext(GlobalContext);
 
+    useEffect(() => {
+        getTransactions();
+        //right now just firing off a warning to end loop, can add: eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
     return (
         <div className="history-container">
             <h2>History</h2>
